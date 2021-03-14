20dca2ea557702ddc5bf004da7d158418651a922
mkulik05 <mishakulik2@gmail.com>
2021-03-14 14:33:48 +0300

    add beautiful bot answer

--- db.js
+++ db.js
@@ -24,7 +24,7 @@ let main = async (
 ) => {
   res = await get_data.data(last_holidays_day, last_quarter_day, quarter_key);
 
-  console.log(res, res.length);
+  //console.log(res, res.length);
   return await run(res).catch(console.dir);
 };
 const mongo_creds = require("./mongo_creds.json");
@@ -56,10 +56,10 @@ let run = async (data) => {
           await col.insertOne(el);
           console.log("add to db");
         } else {
-          console.log(
-            JSON.stringify(result[Object.keys(el)[0]]),
-            JSON.stringify(el[Object.keys(el)[0]])
-          );
+          // console.log(
+          //   JSON.stringify(result[Object.keys(el)[0]]),
+          //   JSON.stringify(el[Object.keys(el)[0]])
+          // );
           if (
             JSON.stringify(result[Object.keys(el)[0]]) !=
             JSON.stringify(el[Object.keys(el)[0]])
@@ -74,19 +74,22 @@ let run = async (data) => {
           }
         }
       } else {
+
+
         let result = await col.findOne({ date: str });
         //console.log(result);
         if (result == null) {
           await col.insertOne(el);
-          changes.push(el);
+          changes.push([{}, el]);
           console.log("add to db");
         } else {
           //console.log(str, result)
           //console.log(JSON.stringify(result), JSON.stringify(el), JSON.stringify(result) == JSON.stringify(el));
           if (!objs_are_simple(result, el)) {
+            //console.log(JSON.stringify(result, null, 4), JSON.stringify(el, null, 4))
+            changes.push([result, el]);
             await col.replaceOne({ date: str }, el);
             console.log("not eq, changesssss");
-            changes.push(el);
           } else {
             console.log("eq");
           }
--- main.js
+++ main.js
@@ -16,20 +16,68 @@ bot.start((ctx) => {
   console.log(ctx.chat.id)
 })
 
+let find_changes = (obj1, obj2, skip_keys = ["_id"]) => {
+  // arr1 - старый, arr2 - новый
+  let changes = {"hw": [], "mark": []}
+  keys1 = Object.keys(obj1);
+  keys2 = Object.keys(obj1);
+  for (let i = 0; i < keys1.length; i++) {
+    key = keys1[i];
+    if (!skip_keys.includes(key)) {
+    if (keys2.includes(key)) {
+      if (JSON.stringify(obj1[key]) != JSON.stringify(obj2[key])) {
+        if (JSON.stringify(obj1[key].mark) != JSON.stringify(obj2[key].mark)) {
+          changes["mark"].push({[key]:obj2[key]})
+        }
+        if (JSON.stringify(obj1[key].hw) != JSON.stringify(obj2[key].hw)) {
+          changes["hw"].push({[key]: obj2[key]})
+        }
+      }
+    }
+  }
+  }
+  return changes
+}
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 
 let check_for_updates = async () => {
   res = await db.update_db(last_holidays_day, last_quarter_day, mongo_creds, 43)
-  for (let i = 0; i < res.length; i++) {
-    bot.telegram.sendMessage(id, res[i])
+  console.log(res, res.length)
+  for(let i = 0; i < res.length; i++){
 
+    let pair = res[i]
+    let date = pair[0].date.split("-")
+    let answ = date[1] + "\-" + date[2] + "\n\n"
+    changes = find_changes(pair[0], pair[1])
+    if (changes.mark.length > 0) {
+      answ += "Новые оценки :\n"
+      for (let i = 0; i < changes.mark.length; i++) {
+        arr = changes.mark[i]
+        answ = answ + " - " + Object.keys(arr)[0].toLowerCase() + "  " + arr[Object.keys(arr)[0]]["mark"] + "\n"
+      }
+      answ+= "\n"
+    }
+    if (changes.hw.length > 0) {
+      answ += "Новое дз :\n"
+      for (let i = 0; i < changes.hw.length; i++) {
+        arr = changes.hw[i]
+        answ = answ + " - " +Object.keys(arr)[0].toLowerCase() + "  " + arr[Object.keys(arr)[0]]["hw"] + "\n"
+      }
+    }
+    bot.telegram.sendMessage(id, answ)
+    //bot.telegram.sendMessage(id, "стало" + JSON.stringify(pair[0], null, 4))
+  
   }
-  //console.log(res)
+  // for (let i = 0; i < res.length; i++) {
+  //   bot.telegram.sendMessage(id, JSON.stringify(res[i], null, 4))
+
+  // }
 }
-setInterval(()=> {
-  check_for_updates()
-}, 30000)
+  //console.log(res)
+
+check_for_updates()
+
 
 bot.launch();
